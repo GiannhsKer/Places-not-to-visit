59467dc52764b2ed79d7367abc6e5af40a951e8f
giannhsker <giannhsker3@gmail.com>
2022-04-29 10:29:57 +0300

    final commit propably

--- App.js
+++ App.js
@@ -3,17 +3,11 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Searchbar } from 'react-native-paper';
-<<<<<<< HEAD
-import Results from './screens/results';
-import Screens from './screens/screens'
-import Display from './screens/display'
-=======
 
 import Screens from './screens/screens'
 import ResultsCities from './screens/resultsCities';
 import Display from './screens/webcamsDisplay'
 import ResultsReviews from './screens/resultsReviews';
->>>>>>> 5977e15aa0cd5d64801da49169c6f6ac256c805f
 
 const Stack = createNativeStackNavigator();
 
@@ -22,20 +16,12 @@ const App = () => {
 
    return (
       <NavigationContainer >
-<<<<<<< HEAD
-        <Stack.Navigator screenOptions = {{headerShown: false}} initialRouteName ='Home'>
-          <Stack.Screen name="Home" component={Home} />
-          <Stack.Screen name="Results" component={Results} />
-          <Stack.Screen name="Screens" component={Screens} />
-          <Stack.Screen name="Display" component={Display} />
-=======
         <Stack.Navigator screenOptions = {{headerShown: false}} initialRouteName ='home'>
           <Stack.Screen name="home" component={Home} />
           <Stack.Screen name="resultsCities" component={ResultsCities} />
           <Stack.Screen name="screens" component={Screens} />
           <Stack.Screen name="display" component={Display} />
           <Stack.Screen name="resultsReviews" component={ResultsReviews} />
->>>>>>> 5977e15aa0cd5d64801da49169c6f6ac256c805f
         </Stack.Navigator>
       </NavigationContainer>
     );
@@ -58,11 +44,7 @@ const Home = ({navigation}) => {
        <View style={styles.button}> 
         <Button
           title = "Search"
-<<<<<<< HEAD
-          onPress= {() => navigation.navigate('Results',{place : query }) }
-=======
           onPress= {() => navigation.navigate('resultsCities',{place : query }) }
->>>>>>> 5977e15aa0cd5d64801da49169c6f6ac256c805f
         />
       </View>
      </View>
